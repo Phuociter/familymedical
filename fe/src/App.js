73e87b2bf34eb5370082f27f6bc1c695b4c97985
Phuociter <phuocv992@gmail.com>
2025-10-25 14:17:30 +0700

    sua lai app js

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Auth/LoginPage.jsx";
 // import DoctorPage from "./pages/DoctorPage";
 import FamilyMedicalPage from "./pages/FamilyMedicalPage.jsx";
@@ -7,22 +7,20 @@ export default function App() {
   const isLoggedIn = localStorage.getItem("token");
 
   return (
-    <Router>
-      <Routes>
-        {/* Login Page */}
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      {/* Login Page */}
+      <Route path="/login" element={<Login />} />
 
-        {/* Protected routes */}
-        {isLoggedIn ? (
-          <>
-            {/* <Route path="/doctors" element={<DoctorPage />} /> */}
-            <Route path="/families" element={<FamilyMedicalPage />} />
-            <Route path="*" element={<Navigate to="/families" />} />
-          </>
-        ) : (
-          <Route path="*" element={<Navigate to="/login" />} />
-        )}
-      </Routes>
-    </Router>
+      {/* Protected routes */}
+      {isLoggedIn ? (
+        <>
+          {/* <Route path="/doctors" element={<DoctorPage />} /> */}
+          <Route path="/families" element={<FamilyMedicalPage />} />
+          <Route path="*" element={<Navigate to="/families" />} />
+        </>
+      ) : (
+        <Route path="*" element={<Navigate to="/login" />} />
+      )}
+    </Routes>
   );
 }
