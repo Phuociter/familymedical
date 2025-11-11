edcc1b9260ee9142076fa1ea27e2d60bec2af33b
Phuociter <phuocv992@gmail.com>
2025-11-11 07:33:47 +0700

    them thanh vien

---
+++ family_medical/src/main/java/com/example/famMedical/dto/UpdateMemberInput.java
@@ -0,0 +1,13 @@
+package com.example.famMedical.dto;
+import lombok.Data;
+
+@Data
+public class UpdateMemberInput {
+
+    private String fullName;
+    
+    private String relationship;
+
+    private String cccd;
+
+}
---
+++ family_medical/src/main/java/com/example/famMedical/resolver/MemberQueryResolver.java
@@ -0,0 +1,40 @@
+package com.example.famMedical.resolver;
+
+import com.example.famMedical.Entity.Member;
+import com.example.famMedical.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+    
+import java.util.List;
+public class MemberQueryResolver {
+    private final MemberService memberService;
+   
+    @Autowired
+    public MemberQueryResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @QueryMapping 
+    public List<Member> membersByFamily(@Argument Integer familyID){
+        return memberService.getMembersByFamilyId(familyID);
+    }
+
+    @MutationMapping
+    public void deleteMember(@Argument Integer memberID){
+         memberService.deleteMember(memberID);
+    }
+
+    @MutationMapping
+    public Member createMember(@Argument Member member){
+        return memberService.createMember(member);
+    }
+
+    @MutationMapping
+    public Member updateMember(@Argument Member member){
+        return memberService.updateMember(member);
+    }
+
+}
